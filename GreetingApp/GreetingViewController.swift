e839f18ea59ea9198e54cbf7c66296ab052eca56
iOSipovDobrynia <dobrynia.osipov@gmail.com>
2023-12-10 17:12:20 +0300

    MVVM

--- GreetingApp/GreetingViewController.swift
+++ GreetingApp/GreetingViewController.swift
@@ -11,12 +11,23 @@ class GreetingViewController: UIViewController {
 
     @IBOutlet var greetingLabel: UILabel!
     
+    private var viewModel: GreetingViewModelProtocol! {
+        didSet {
+            viewModel.greetingDidChange = { [unowned self] viewModel in
+                greetingLabel.text = viewModel.greeting
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let person = Person(name: "Foo", surname: "Bar")
+        viewModel = GreetingViewModel(person: person)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
+        viewModel.showGreeting()
     }
 
 }
